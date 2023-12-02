7f8041b16021ea3c4ffc1248064fd9a6d67c3133
KR9SIS <kristoferhelgi@protonmail.com>
2023-12-02 02:14:29 +0000

    Right numbers, wrong order

--- Day_1/1.py
+++ Day_1/1.py
@@ -1,18 +1,59 @@
-with open(r"Day_1\1_input.txt", "r") as file:
-    first_last = []
-    for line in file:
-        numbers = []
-        for letter in line:
-            try:
-                number = int(letter)
-                numbers.append(number)
-            except:
-                """"""
-        first = str(numbers[0])
-        last = str(numbers[-1])
-        comp = first + last
-        comp = int(comp)
-        first_last.append(comp)
+filename = "1_input.txt"
+# filename = "example.txt"
+digits = [
+    "0",
+    "zero",
+    "1",
+    "one",
+    "2",
+    "two",
+    "3",
+    "three",
+    "4",
+    "four",
+    "5",
+    "five",
+    "6",
+    "six",
+    "7",
+    "seven",
+    "8",
+    "eight",
+    "9",
+    "nine",
+]
 
-answer = sum(first_last)
-print(answer)
+
+def main(filename):
+    file_num = check_file(filename)
+    answer = sum(file_num)
+    print(answer)
+
+
+def check_file(filename):
+    with open(filename, "r") as file:
+        numbers = []
+        for line in file:
+            l_num = check_line(line)
+            numbers.append(l_num)
+        return numbers
+
+
+def check_line(line):
+    line_num: list[str] = []
+    for digit in digits:
+        if digit in line:
+            try:
+                int(digit)
+                line_num.append(digit)
+            except:
+                index = digits.index(digit)
+                line_num.append(digits[index - 1])
+    first = line_num[0]
+    last = line_num[-1]
+    firstNlast = first + last
+    return int(firstNlast)
+
+
+if __name__ == "__main__":
+    main(filename)
