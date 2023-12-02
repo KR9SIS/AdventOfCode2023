3be8eb5efda6a14de2b6d839e0bbf7ce364aae5a
KR9SIS <kristoferhelgi@protonmail.com>
2023-12-02 23:08:08 +0000

    Continued work and set up a file for tries

--- Day_1/1.py
+++ Day_1/1.py
@@ -1,49 +1,108 @@
 # filename = "1_input.txt"
 filename = "example.txt"
 
-ZERO = set(("0", "zero"))
-ONE = set(("1", "one"))
-TWO = set(("2", "two"))
-THREE = set(("3", "three"))
-FOUR = set(("4", "four"))
-FIVE = set(("5", "five"))
-SIX = set(("6", "six"))
-SEVEN = set(("7", "seven"))
-EIGHT = set(("8", "eight"))
-NINE = set(("9", "nine"))
-DIGITS = set()
-DIGITS.update(ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE)
+DIGITS = {
+    "zero": ["0", "zero"],
+    "one": ["1", "one"],
+    "two": ["2", "two"],
+    "three": ["3", "three"],
+    "four": ["4", "four"],
+    "five": ["5", "five"],
+    "six": ["6", "six"],
+    "seven": ["7", "seven"],
+    "eight": ["8", "eight"],
+    "nine": ["9", "nine"],
+}
+PARTS = {
+    "zero": set(("z", "ze", "zer")),
+    "one": set(("o", "on")),
+    "two": set(("t", "tw")),
+    "three": set(("t", "th", "thr", "thre")),
+    "four": set(("f", "fo", "fou")),
+    "five": set(("f", "fi", "fiv")),
+    "six": set(("s", "si")),
+    "seven": set(("s", "se", "sev", "seve")),
+    "eight": set(("e", "ei", "eig", "eigh")),
+    "nine": set(("n", "ni", "nin")),
+}
 
 
 def main(filename):
-    file_num = check_file(filename)
-    answer = sum(file_num)
+    content = get_file(filename)
+    numbers = []
+    for line in content:
+        l_num = check_num(line)
+        n_order = check_order(line, l_num)
+        f_l = first_last(n_order)
+        numbers.append(f_l)
+    answer = sum(numbers)
     print(answer)
 
 
-def check_file(filename):
-    with open(filename, "r") as file:
-        numbers = []
+def get_file(filename):
+    with open(filename) as file:
+        content = []
         for line in file:
-            l_num = check_line(line)
-            numbers.append(l_num)
-        return numbers
+            content.append(line)
+    return content
 
 
-def check_line(line):
-    letters = ""
-    line_num: list[str] = []
-    line = set((line))
-    line = line.intersection(DIGITS)
+def check_num(line):
+    line_num = []
+    for number in DIGITS.values():
+        for version in number:
+            if version in line:
+                line_num.append(version)
+    ""
+    return line_num
 
-    first = line_num[0]
-    last = line_num[-1]
+
+def check_order(line, numbers: list[str]):
+    """Checks the order of the numbers found by iterating over the line and matching the number in the line with the one"""
+    num_order = []
+    n_parts = set()
+    for key in numbers:
+        try:
+            n_parts = n_parts.union(PARTS[key])
+        except:
+            """"""
+    letters: str = ""
+    for char in line:
+        letters += char
+        if letters in numbers:
+            num_order.append(letters)
+            if letters[-1] not in n_parts:
+                letters = ""
+            else:
+                letters = letters[-1]
+        elif letters in n_parts:
+            pass
+        elif letters[-1] in numbers:
+            num_order.append(letters[-1])
+            letters = ""
+        elif letters[-1] in n_parts:
+            letters = letters[-1]
+
+        else:
+            letters = ""
+    return num_order
+
+
+def first_last(numbers):
+    integers = []
+    for i in numbers:
+        try:
+            int(i)  # Check whether the element is i.e. a "1" or a "one"
+            integers.append(i)
+        except:
+            num = DIGITS[i][0]
+            integers.append(num)
+
+    first = integers[0]
+    last = integers[-1]
     firstNlast = first + last
     return int(firstNlast)
 
 
 if __name__ == "__main__":
     main(filename)
-
-    char = ""
-    char.isdigit()
