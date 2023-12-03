157accc216d0fd84d1d46ebc706e43a420f1fb54
KR9SIS <144333573+KR9SIS@users.noreply.github.com>
2023-12-03 11:40:46 +0000

    Day1 (#1)
    * Right numbers, wrong order
    
    * Did some work on the same problem
    
    * Continued work and set up a file for tries
    
    * Sorted tries
    
    * Renamed copy to class and noted another try
    
    * Added function docstrings
    
    * Added input explanations for 1 - class.py
    
    * Added input to gitignore
    
    * Finished Day1
    
    * Updated file
    
    * Tried to uncache the two files for the .gitignore

---
+++ Day_1/1,2 - class.py
@@ -0,0 +1,134 @@
+class numFinder:
+    def __init__(self, file: str):
+        self.content: list[str] = []  # Every line in the file
+        self.line: str = ""
+        self.line_num: set[str] = set()  # Every number in a line
+        self.num_ord: list[str] = []  # The order of the numbers in a line
+        self.file_num: list[int] = []  # Every lines first and last digit put together
+
+        self.DIGITS: dict[str, list[str]] = {
+            "zero": ["0", "zero"],
+            "one": ["1", "one"],
+            "two": ["2", "two"],
+            "three": ["3", "three"],
+            "four": ["4", "four"],
+            "five": ["5", "five"],
+            "six": ["6", "six"],
+            "seven": ["7", "seven"],
+            "eight": ["8", "eight"],
+            "nine": ["9", "nine"],
+        }
+        self.PARTS: dict[str, set[str]] = {
+            "zero": set(("z", "ze", "zer")),
+            "one": set(("o", "on")),
+            "two": set(("t", "tw")),
+            "three": set(("t", "th", "thr", "thre")),
+            "four": set(("f", "fo", "fou")),
+            "five": set(("f", "fi", "fiv")),
+            "six": set(("s", "si")),
+            "seven": set(("s", "se", "sev", "seve")),
+            "eight": set(("e", "ei", "eig", "eigh")),
+            "nine": set(("n", "ni", "nin")),
+        }
+
+        self.get_file_content(file)
+        for line in self.content:
+            self.line = line
+            self.get_num()
+            if len(self.line_num) > 1:
+                self.get_num_ord()
+            else:
+                self.num_ord = list(self.line_num)
+            self.first_last()
+        self.show_sum()
+
+    def get_file_content(self, file):
+        """
+        Opens a file and reads it's contents into a list
+        """
+        with open(filename) as file:
+            for line in file:
+                self.content.append(line)
+
+    def get_num(self):
+        """
+        Finds every number contained in a line
+        """
+        self.line_num = set()
+        for number in self.DIGITS.values():
+            for version in number:
+                if version in self.line:
+                    self.line_num.add(version)
+
+    def get_num_ord(self):
+        """
+        Adds every number in a line to a list using accounting for these cases
+        Case:
+            1. Normal "f1d" extracts 1
+            2. Word "feightg" extracts eight
+            3. Digit in word "ei9ht" extracts 9
+            4. Word in word "oneight" extracts one & eight
+        """
+        self.num_ord = []
+        n_parts = set()
+        for key in self.line_num:
+            try:
+                n_parts = n_parts.union(self.PARTS[key])
+            except:
+                """"""
+
+        curr_word = ""
+        for char in self.line:
+            if char in self.line_num:  # Case 1 & 3
+                self.num_ord.append(char)  # Character is a digit
+                curr_word = ""
+            if char.isalpha():  # Case 2 & 4
+                curr_word += char
+                if curr_word in self.line_num:  # Case 2
+                    self.num_ord.append(curr_word)
+
+                    if curr_word[-1] in n_parts:
+                        curr_word = curr_word[-1]  # Case 4
+                    else:
+                        curr_word = ""
+
+                elif curr_word in n_parts:  # Poosible case 2
+                    pass
+
+                elif curr_word[-1] in n_parts:
+                    curr_word = curr_word[-1]  # Possible case 4
+
+                else:
+                    curr_word = ""  # Not a case
+
+    def first_last(self):
+        """
+        Combines the first and last number in a line into a single combined integer
+        """
+        integers = []
+        for i in self.num_ord:
+            try:
+                int(i)  # Check whether the element is i.e. a "1" or a "one"
+                integers.append(i)
+            except:
+                num = self.DIGITS[i][0]
+                integers.append(num)
+
+        first = integers[0]
+        last = integers[-1]
+        firstNlast = first + last
+        self.file_num.append(int(firstNlast))
+
+    def show_sum(self):
+        """
+        calculates the sum of all combined integers
+        """
+        answer = sum(self.file_num)
+        print(answer)
+
+
+if __name__ == "__main__":
+    filename = "1_input.txt"  # Input for day 1 part 1
+    # filename = "example.txt"  # Specific line which I wanted to tackle
+
+    lookup = numFinder(filename)
--- Day_1/1.py
+++ Day_1/1.py
@@ -1,18 +1,109 @@
-with open(r"Day_1\1_input.txt", "r") as file:
-    first_last = []
-    for line in file:
-        numbers = []
-        for letter in line:
-            try:
-                number = int(letter)
-                numbers.append(number)
-            except:
-                """"""
-        first = str(numbers[0])
-        last = str(numbers[-1])
-        comp = first + last
-        comp = int(comp)
-        first_last.append(comp)
+filename = "1_input.txt"
+# filename = "example.txt"
 
-answer = sum(first_last)
-print(answer)
+DIGITS = {
+    "zero": ["0", "zero"],
+    "one": ["1", "one"],
+    "two": ["2", "two"],
+    "three": ["3", "three"],
+    "four": ["4", "four"],
+    "five": ["5", "five"],
+    "six": ["6", "six"],
+    "seven": ["7", "seven"],
+    "eight": ["8", "eight"],
+    "nine": ["9", "nine"],
+}
+PARTS = {
+    "zero": set(("z", "ze", "zer")),
+    "one": set(("o", "on")),
+    "two": set(("t", "tw")),
+    "three": set(("t", "th", "thr", "thre")),
+    "four": set(("f", "fo", "fou")),
+    "five": set(("f", "fi", "fiv")),
+    "six": set(("s", "si")),
+    "seven": set(("s", "se", "sev", "seve")),
+    "eight": set(("e", "ei", "eig", "eigh")),
+    "nine": set(("n", "ni", "nin")),
+}
+
+
+def main(filename):
+    content = get_file(filename)
+    numbers = []
+    for line in content:
+        l_num = check_num(line)
+        n_order = check_order(line, l_num)
+        f_l = first_last(n_order)
+        numbers.append(f_l)
+    answer = sum(numbers)
+    print(answer)
+
+
+def get_file(filename):
+    with open(filename) as file:
+        content = []
+        for line in file:
+            line = line.lower()
+            content.append(line)
+    return content
+
+
+def check_num(line):
+    line_num = []
+    for number in DIGITS.values():
+        for version in number:
+            if version in line:
+                line_num.append(version)
+    ""
+    return line_num
+
+
+def check_order(line, numbers: list[str]):
+    """Checks the order of the numbers found by iterating over the line and matching the number in the line with the one"""
+    num_order = []
+    n_parts = set()
+    for key in numbers:
+        try:
+            n_parts = n_parts.union(PARTS[key])
+        except:
+            """"""
+    letters: str = ""
+    for char in line:
+        letters += char
+        if letters in numbers:
+            num_order.append(letters)
+            if letters[-1] not in n_parts:
+                letters = ""
+            else:
+                letters = letters[-1]
+        elif letters in n_parts:
+            pass
+        elif letters[-1] in numbers:
+            num_order.append(letters[-1])
+            letters = ""
+        elif letters[-1] in n_parts:
+            letters = letters[-1]
+
+        else:
+            letters = ""
+    return num_order
+
+
+def first_last(numbers):
+    integers = []
+    for i in numbers:
+        try:
+            int(i)  # Check whether the element is i.e. a "1" or a "one"
+            integers.append(i)
+        except:
+            num = DIGITS[i][0]
+            integers.append(num)
+
+    first = integers[0]
+    last = integers[-1]
+    firstNlast = first + last
+    return int(firstNlast)
+
+
+if __name__ == "__main__":
+    main(filename)
