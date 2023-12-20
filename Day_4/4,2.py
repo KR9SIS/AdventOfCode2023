82e14a933201787003f01d7bd6f178a5d8483637
KR9SIS <kristoferhelgi@protonmail.com>
2023-12-20 20:04:10 +0100

    Started work on 4,2

--- Day_4/4,2.py
+++ Day_4/4,2.py
@@ -1,22 +1,33 @@
-filename = "4_input.txt"
-# filename = "example.txt"
+# filename = "4_input.txt"
+filename = "example.txt"
 
 
 class Scratchcards:
+    """
+    Game rules:
+        if card 1 has four matches, then you get A copy of cards 2 til 5
+        Then you calculate the copies created by your copies. Keep doing this until no more copies are generated.
+
+    """
+
     def __init__(self) -> None:
-        self.cards: dict[str, tuple[list[int]]] = {}
+        self.cards: dict[int, tuple[list[int]]] = {}
+        self.copies: dict[int, int] = {}
+        self.matches: dict[int, int] = {}
         self.card_points: list[int] = []
 
-        self.get_cards()
-        self.calc_win()
+        self.get_cards_n_copies()
+        self.calc_matches()
         points = sum(self.card_points)
         print(points)
 
-    def get_cards(self):
+    def get_cards_n_copies(self):
         with open(filename) as file:
             for line in file:
                 front, numbers = line.split("|")
                 card, win_num = front.split(":")
+                _, card = card.split()
+                card = int(card)
 
                 numbers = numbers.strip().split()
                 win_num = win_num.strip().split()
@@ -25,16 +36,22 @@ class Scratchcards:
                 win_num = list(map(int, win_num))
 
                 self.cards[card] = (win_num, numbers)
+                self.copies[card] = 0
 
-    def calc_win(self):
+    def calc_matches(self):
         for card, numbers in self.cards.items():
             matches = 0
             win_num, num = numbers
             for win in win_num:
                 if win in num:
                     matches += 1
-            self.card_points.append(matches)
+            self.matches[card] = matches
             """"""
+
+    def calc_wins(self):
+        for card, matches in self.matches.items():
+            pass
+            # for win in pass:
 
 
 if __name__ == "__main__":
