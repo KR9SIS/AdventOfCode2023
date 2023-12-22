fab4328b7da47feed899b11ec1c8efd458fe5fd4
KR9SIS <kristoferhelgi@protonmail.com>
2023-12-22 15:06:28 +0100

    Continued work on 4,2

--- Day_4/4,2.py
+++ Day_4/4,2.py
@@ -11,17 +11,15 @@ class Scratchcards:
     """
 
     def __init__(self) -> None:
-        self.cards: dict[int, tuple[list[int]]] = {}
-        self.copies: dict[int, int] = {}
-        self.matches: dict[int, int] = {}
-        self.card_points: list[int] = []
+        self.cards: dict[int, tuple[list[int], list[int], list[int], list[int]]] = {}
+        self.copies: list[int] = []
+        self.count = 0
 
-        self.get_cards_n_copies()
-        self.calc_matches()
-        points = sum(self.card_points)
-        print(points)
+        self.get_cards()
+        self.card_counter()
+        print(self.count)
 
-    def get_cards_n_copies(self):
+    def get_cards(self):
         with open(filename) as file:
             for line in file:
                 front, numbers = line.split("|")
@@ -35,23 +33,61 @@ class Scratchcards:
                 numbers = list(map(int, numbers))
                 win_num = list(map(int, win_num))
 
-                self.cards[card] = (win_num, numbers)
-                self.copies[card] = 0
+                self.cards[card] = (win_num, numbers, [], [])
 
-    def calc_matches(self):
-        for card, numbers in self.cards.items():
-            matches = 0
-            win_num, num = numbers
+    def card_counter(self):
+        for cur_card in self.cards:
+            # self.count = 0
+            # self.check_summary()
+            self.copies.append(cur_card)
+            while self.copies:
+                self.calc_copies()
+            self.summarise(cur_card)
+
+    def check_summary(self):
+        check = self.copies[0]
+        raise NotImplemented
+
+    def calc_copies(self):
+        self.count += 1
+        card = self.copies.pop(0)
+        win_num, num, win_list, summary = self.cards[card]
+
+        if win_list:
+            self.copies.extend(win_list)
+            # self.count += summary
+
+        else:
+            wins = 0
             for win in win_num:
                 if win in num:
-                    matches += 1
-            self.matches[card] = matches
-            """"""
+                    wins += 1
+            if wins:
+                copies = card
+                for i in range(wins):
+                    copies += 1
+                    self.copies.append(copies)
+                    win_list.append(copies)
 
-    def calc_wins(self):
-        for card, matches in self.matches.items():
-            pass
-            # for win in pass:
+                self.cards[card] = win_num, num, win_list, summary
+
+    def summarise(self, current_card):
+        """
+        Go through every copy created by the previous card
+
+        """
+        card_points = 0
+        win_num, num, copies, total = self.cards[current_card]
+        for copy in reversed(copies):
+            copy_win_num, copy_num, copy_copies, copy_points = self.cards[copy]
+            card_points += len(copy_copies) + 1
+            copy_points = card_points
+
+            self.cards[copy] = copy_win_num, copy_num, copy_copies, copy_points
+
+        card_points += len(copies)
+        self.cards[current_card] = win_num, num, copies, card_points
+        # self.count += card_points
 
 
 if __name__ == "__main__":
